28cfac145bbadd19e29726530d689a9c69875610
ridoy113 <almamounhridoy113@gmail.com>
2022-11-17 18:08:02 +0600

    making start hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Hero } from './components'
+import { heroapi } from './data/data';
 
 const App = () => {
   return (
     <div>
       <main>
-        <Hero />
+        <Hero heroapi={heroapi} />
       </main>
     </div>
   )
