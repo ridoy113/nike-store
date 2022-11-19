2558053ae1acacd760fa5eebba1e5fa04807225e
ridoy113 <almamounhridoy113@gmail.com>
2022-11-19 15:39:01 +0600

    add custom background colar

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -6,17 +6,17 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
 
     return (
         <>
-            <div>
-                <div>
-                    <h1>{title}</h1>
-                    <p>{text}</p>
-                    <div>
-                        <div><h1>{price}</h1></div>
-                        <div><StarIcon
+            <div className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center`}>
+                <div className=''>
+                    <h1 className=''>{title}</h1>
+                    <p className=''>{text}</p>
+                    <div className=''>
+                        <div className=''><h1 className=''>{price}</h1></div>
+                        <div className=''><StarIcon
                             className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight text-slate-900' />
-                            <h1>{rating}</h1></div>
+                            <h1 className=''>{rating}</h1></div>
                     </div>
-                    <div>
+                    <div className=''>
                         <button type='button' className=''><ShoppingBagIcon className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight text-slate-900' /></button>
                         <button type='button' className=''>{btn}</button>
                     </div>
