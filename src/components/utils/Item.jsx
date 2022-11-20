b15c509b8258fc5282a727f4ffeced4a3820fd28
ridoy113 <almamounhridoy113@gmail.com>
2022-11-20 21:33:05 +0600

    popular product data class added

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -13,16 +13,16 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
 
 
 
-                    <div className='flex items-center justify-between w-28 '>
-                        <div className='flex items-center bg-white/80 px-1 rounded'><h1 className='text-black text-sm'>${price}</h1></div>
-                        <div className='flex items-center'><StarIcon
-                            className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight text-slate-900' />
-                            <h1 className=''>{rating}</h1></div>
+                    <div className='flex items-center justify-between w-28 my-2'>
+                        <div className='flex items-center bg-white/80 px-1 rounded blur-effect-theme'><h1 className='text-black text-sm font-medium'>${price}</h1></div>
+                        <div className='flex items-center gap-1'><StarIcon
+                            className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight w-5 h-5 md:w-4 md:h-4 ' />
+                            <h1 className='md:text-sm font-normal text-slate-100'>{rating}</h1></div>
                     </div>
 
-                    <div className='flex items-center'>
-                        <button type='button' className=''><ShoppingBagIcon className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight text-slate-900' /></button>
-                        <button type='button' className=''>{btn}</button>
+                    <div className='flex items-center gap-3'>
+                        <button type='button' className='bg-white/90 blur-effect-theme py-1 rounded active:scale-90 transition-all duration-100 ease-in-out shadow-md sm:text-sm p-0.5 shadow shadow-slate-200'><ShoppingBagIcon className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight text-slate-900' /></button>
+                        <button type='button' className='bg-white/90 blur-effect-theme px-7 py-1 rounded active:scale-90 transition-all duration-100 ease-in-out shadow-md sm:text-sm px-2 py-1 shadow shadow-slate-200 text-sm text-black'>{btn}</button>
                     </div>
                 </div>
 
