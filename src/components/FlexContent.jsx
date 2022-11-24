38e01815abbc844360ebf446f8821d1e44e52de2
ridoy113 <almamounhridoy113@gmail.com>
2022-11-24 10:34:10 +0600

    working on flexcontent section

--- src/components/FlexContent.jsx
+++ src/components/FlexContent.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const FlexContent = () => {
     return (
-        <div>FlexContent</div>
+        <>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+        </>
     )
 }
 
