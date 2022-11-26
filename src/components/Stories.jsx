a56130047b1dbddcc11671c18480ad5efe10a32d
ridoy113 <almamounhridoy113@gmail.com>
2022-11-26 18:31:10 +0600

    splide slider added

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,5 +1,9 @@
-import React from 'react'
-import Title from './utils/Title'
+import React from 'react';
+import Title from './utils/Title';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+
+
 
 const Stories = ({ story: { title, news, } }) => {
     return (
@@ -7,7 +11,17 @@ const Stories = ({ story: { title, news, } }) => {
             <div className='nike-container mb-11'>
                 <Title title={title} />
                 <div>
-                    
+                    <Splide>
+                        {news.map((val, i) => (
+                            <SplideSlide key={i}
+                                className="mb-0.5"
+                            >
+                                <div>
+                                    <div><img src={val.img} alt={`img/story/${i}`} className="" /></div>
+                                </div>
+                            </SplideSlide>
+                        ))}
+                    </Splide>
                 </div>
             </div>
         </>
