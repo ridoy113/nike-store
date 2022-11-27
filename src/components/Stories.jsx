12ee963446559edd0a97a00b61b1a23d54272c4f
ridoy113 <almamounhridoy113@gmail.com>
2022-11-27 11:25:53 +0600

    sories section making stylish

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -13,20 +13,30 @@ const Stories = ({ story: { title, news, } }) => {
         <>
             <div className='nike-container mb-11'>
                 <Title title={title} />
-                <div>
+                <div className=''>
                     <Splide>
                         {news.map((val, i) => (
                             <SplideSlide key={i}
                                 className="mb-0.5"
                             >
-                                <div>
-                                    <div><img src={val.img} alt={`img/story/${i}`} className="" /></div>
-                                    <div>
-                                        <div><HeartIcon className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight' /><span>{val.like}</span></div>
+                                <div className='relative grid items-center gap-4 pb-2 rounded-lg shadow shadow-slate-200 ring-1 ring-slate-200'>
+                                    <div className='flex items-center justify-center'><img src={val.img} alt={`img/story/${i}`} className="w-full h-auto object-cover shadow-md shadow-slate-200 rounded-tl-lg rounded-tr-lg" /></div>
 
-                                        <div><ClockIcon className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight' /><span>{val.time}</span></div>
+                                    <div className='flex items-center justify-between w-full px-4'>
+                                        <div className=''><HeartIcon className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight' /><span>{val.like}</span></div>
 
-                                        <div><HashtagIcon className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight' /><span>{val.by}</span></div>
+                                        <div className=''><ClockIcon className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight' /><span>{val.time}</span></div>
+
+                                        <div className=''><HashtagIcon className='h-6 w-6 sm: h-5 sm:w-5 text-slate-100 cursor-pointer transition-all duration-100 active:scale-110 leading-tight' /><span>{val.by}</span></div>
+                                    </div>
+
+                                    <div className=''>
+                                        <h1>{val.title}</h1>
+                                        <p>{val.text}</p>
+                                    </div>
+
+                                    <div className=''>
+                                        <a href={val.url}>{val.btn}</a>
                                     </div>
                                 </div>
                             </SplideSlide>
