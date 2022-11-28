087cdf786ba9a7800243c09254cf6376d3cfd855
ridoy113 <almamounhridoy113@gmail.com>
2022-11-28 19:08:37 +0600

    input all data on navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import{HeartIcon, MagnifyingGlassIcon, ShoppingBagIcon} from '@heroicons/react/24/outline'
+import logo from '../assets/logo.png'
+
 
 const Navbar = () => {
   return (
-    <div>
-      tghh
-    </div>
+    <>
+      <header>
+        <nav>
+            <div>
+                <img src={logo} alt="logo/img" className='' />
+            </div>
+            <ul>
+                <li>
+                    <MagnifyingGlassIcon className='icon-style'/>
+                </li>
+                <li>
+                    <HeartIcon className='icon-style'/>
+                </li>
+                <li>
+                    <button type='button' className=''>
+                        <ShoppingBagIcon className='icon-style'/>
+                        <div>
+                            0
+                        </div> 
+                    </button>
+                </li>
+            </ul>
+        </nav>
+      </header>
+    </>
   )
 }
 
